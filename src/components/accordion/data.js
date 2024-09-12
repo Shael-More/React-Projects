26c445a2be3de16891491d689198f492e1a8064c
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-09-12 11:26:01 +0530

    Accordion Project with multi selection

--- src/components/accordion/data.js
+++ src/components/accordion/data.js
@@ -1,23 +1,23 @@
 const data = [
     {
         id: 1,
-        question:"Question 1",
-        answer: "Answers will show here on button toggle",
+        question:"Section 1",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id: 2,
-        question:"Question 2",
-        answer: "Answers will show here on button toggle",
+        question:"Section 2",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id: 3,
-        question:"Question 3",
-        answer: "Answers will show here on button toggle",
+        question:"Section 3",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id: 4,
-        question:"Question 4",
-        answer: "Answers will show here on button toggle",
+        question:"Section 4",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
 ]
 
--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,38 +3,62 @@ import data from './data';
 
 const Accordion = () => {
   const [singleSelection, setSingleSelection] = useState(null);
+  const [enableMultiSel, setEnableMultiSel] = useState(false);
+  const [multiSelected, setMultiSelected] = useState([]);
 
-  const handleSingleSelection = (getCurrentId) => {
-    console.log(getCurrentId);
-
-    setSingleSelection(singleSelection === getCurrentId ? null : getCurrentId);
-
-    // console.log(singleSelection);
+  const handleSingleSelection = (currentId) => {
+    setSingleSelection(currentId === singleSelection ? null : currentId);
   };
+
+  const handleMultiSelection = (currentId) => {
+    let cpyMultiple = [...multiSelected];
+
+    const findIndexOfCurrentId = cpyMultiple.indexOf(currentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(currentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiSelected(cpyMultiple);
+  };
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center flex-col bg-blue-300'>
-      <div className='mb-4 text-2xl '>Accordions</div>
-      <div className='w-[500px] flex flex-col justify-center items-center '>
+    <div className='flex justify-center items-center flex-col gap-5 h-screen w-screen'>
+      <h1>Multi Selection</h1>
+      <button
+        onClick={() => setEnableMultiSel(!enableMultiSel)}
+        className='bg-blue-300 p-2 rounded-sm hover:bg-blue-500'
+      >
+        {!enableMultiSel ? "Enable" : "Disable" }
+      </button>
+      <div className='w-[500px]'>
         {data && data.length > 0 ? (
           data.map((item) => (
-            <div
-              key={item.id}
-              className='flex flex-col w-full p-4 bg-red-400 text-white'
-              onClick={() => handleSingleSelection(item.id)}
-            >
-              <div className='flex justify-between w-full'>
-                <h3 className='text-2xl'>{item.question}</h3>
+            <div key={item.id} className='bg-slate-300 mb-2 p-2'>
+              <div
+                className='flex justify-between font-bold cursor-pointer'
+                onClick={
+                  enableMultiSel
+                    ? () => handleMultiSelection(item.id)
+                    : () => handleSingleSelection(item.id)
+                }
+              >
+                <h3>{item.question}</h3>
                 <span>+</span>
               </div>
-              <div className='text-center'>
-                {singleSelection === item.id ? (
-                  <h2 className='text-2xl p-2'>{item.answer}</h2>
-                ) : null}
-              </div>
+              {enableMultiSel
+                ? multiSelected.indexOf(item.id) !== -1 && (
+                    <div className='p-2 h-auto'>{item.answer}</div>
+                  )
+                : singleSelection === item.id && (
+                    <div className='p-2 h-auto'>{item.answer}</div>
+                  )}
+
+              {/* {singleSelection === item.id || multiSelected.indexOf(item.id) !== -1 ? (
+                <p className='p-2 h-auto'>{item.answer}</p>
+              ) : null} */}
             </div>
           ))
         ) : (
-          <div>Data not found!</div>
+          <div>No data found!</div>
         )}
       </div>
     </div>
