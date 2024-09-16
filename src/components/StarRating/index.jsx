0df56889d5400bdc771d99f1dda48bedb351a08f
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-09-16 20:13:56 +0530

    Star Rating

---
+++ src/components/StarRating/index.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import { FaStar } from 'react-icons/fa'
+
+const StarRating = ({noOfStars = 5}) => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+
+    const handleClick = (currentId) => {
+        setRating(currentId);
+    }
+    const handleMouseEnter = (currentId) => {
+        setHover(currentId);
+    }
+    const handleMouseLeave = () => {
+        setHover(rating)
+    }
+  return (
+    <div className='flex '>
+    {
+        [ ...Array(noOfStars)].map((_, index) => {
+            index += 1
+            return <FaStar
+            key={index}
+            className={index <= (hover || rating) ? 'text-orange-' : 'text-black' }
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={80}
+            />
+        })
+    }
+    </div>
+  )
+}
+
+export default StarRating;
