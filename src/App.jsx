10d886e4722f47419014eecb14cf2968d4bb2523
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-09-28 22:23:07 +0530

    main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import './App.css';
+import Accordion from './components/Accordion';
+import RandomColor from './components/RandomColorGenerator';
+import StarRating from './components/StarRating';
+import ImageSlider from './components/ImageSlider';
+import LoadItems from './components/LoadItems';
+import Responsive from './components/ResponsiveJSX';
+import TreeView from './components/TreeViewUI';
+import Menus from './components/TreeViewUI/data';
+import QRCodeGenerator from './components/QR-CODE-Generator';
+import ReactForm from './Testing/ReactForm';
+import FirstHook from './Testing/usestate';
+import ThemeToggle from './components/ThemeToggle';
+import ScrollIndicator from './components/Scroll-Indication';
+import ParentTab from './components/Custom-Tabs/parent-tab';
+import ParentModal from './components/Custom-modal-popup/parentmodal';
+import ProfileFinder from './components/Github-Profile-Finder';
+import SearchAutocomplete from './components/Search-Autocomplete';
+import TicTacToe from './components/TicTacToe';
 
 function App() {
   return (
     <>
-      <h1>HELLO</h1>
+      {/* Accordion Component */}
+      {/* <Accordion /> */}
+      {/* Random Color Generator Component */}
+      {/* <RandomColor /> */}
+      {/* Star Rating Component */}
+      {/* <StarRating noOfStars={10}/> */}
+      {/* Image Slider Component */}
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} /> */}
+      {/* Load more data component */}
+      {/* <LoadItems /> */}
+      {/* Responsive component */}
+      {/* <Responsive />  */}
+      {/* Tree View UI component */}
+      {/* <TreeView menus={Menus} /> */}
+      {/* QR Code Generator Component */}
+      {/* <QRCodeGenerator /> */}
+      {/* Theme Toggle Component */}
+      {/* <ThemeToggle /> */}
+      {/* Scroll Indicator Component */}
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
+       */}
+      {/* Custom Tab with dynamic content component */}
+      {/* <ParentTab /> */}
+      {/* Custom Modal Popup */}
+      {/* <ParentModal /> */}
+      {/* Github Profile Finder Component */}
+      {/* <ProfileFinder /> */}
+      {/* Search Autocomplete Component */}
+      {/* <SearchAutocomplete /> */}
+      {/* Tic Tac Toe  */}
+      <TicTacToe />
+
+      {/* React Form */}
+      {/* <ReactForm /> */}
+      {/* React All Hooks  */}
+      {/* <FirstHook /> */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,34 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Router Tutorial cosden
+
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import HomePage from './pages/HomePage.jsx';
+// import ProfilesPage from './pages/ProfilesPage.jsx';
+// import NotFoundPage from './pages/NotFoundPage.jsx';
+// import ProfilePage from './pages/ProfilePage.jsx';
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//     errorElement: <NotFoundPage />,
+//   },
+//   {
+//     path: '/profiles',
+//     element: <ProfilesPage />,
+//     children: [
+//       {
+//         path: '/profiles/:profileId',
+//         element: <ProfilePage />,
+//       },
+//     ],
+//   },
+// ]);
+
 createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>
-  // <StrictMode>
-  // </StrictMode>,
+  <StrictMode>
+    {/* <RouterProvider router={router} /> */}
+    <App /> 
+  </StrictMode>
 );
